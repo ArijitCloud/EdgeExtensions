bf83400ae63d636036a753e4de71b8d624b4efab
Arijit <81144422+ArijitCloud@users.noreply.github.com>
2023-09-01 16:36:24 +0200

    Feat: Add option to use storage and refactoring using es6 module (#3)
    * add option to use storage and refactoring using es6 module
    
    * update manifest
    
    * manifest update

--- UrlEncoderDecoder/popup/popup.js
+++ UrlEncoderDecoder/popup/popup.js
@@ -1,31 +1,98 @@
-const inputAreaId = document.getElementById("input-area");
-const outputAreaId = document.getElementById("output-area");
-const buttonSwitch = document.getElementById("switch");
-const toolName = document.getElementById("utility-name");
-if (inputAreaId && outputAreaId) {
-  inputAreaId.oninput = function () {
-    outputAreaId.value =
-      buttonSwitch.innerText === textContent.switchEncoder
-        ? decodeURIComponent(inputAreaId.value)
-        : encodeURIComponent(inputAreaId.value);
-  };
-}
+import {
+  getFromStorage,
+  saveInStorage,
+  removeFromStorage,
+} from "./useStorage.js";
+import { decodeOrEncodeUrl } from "./utility.js";
 
-if (buttonSwitch) {
-  buttonSwitch.onclick = function () {
-    if (buttonSwitch.innerText === textContent.switchEncoder) {
-      buttonSwitch.innerText = textContent.switchDecoder;
-      toolName.innerText = textContent.encoderTool;
-    } else {
-      buttonSwitch.innerText = textContent.switchEncoder;
-      toolName.innerText = textContent.decoderTool;
-    }
-  };
-}
-
+/**
+ * Text content for UI elements and constants
+ */
 const textContent = {
   switchEncoder: "Switch to encoder",
   switchDecoder: "Switch to decoder",
   encoderTool: "Url Encoder",
   decoderTool: "Url Decoder",
 };
+const storageKey = "decoderOrEncoder";
+
+/**
+ * Read DOM elements
+ */
+const inputAreaId = document.getElementById("input-area");
+const outputAreaId = document.getElementById("output-area");
+const buttonSwitch = document.getElementById("switch");
+const toolName = document.getElementById("utility-name");
+const checkboxUseStorage = document.getElementById("use-storage");
+
+/**
+ * Once all required DOM elements are available perform desired operation
+ */
+if (inputAreaId && outputAreaId && buttonSwitch) {
+  const storedValue = getFromStorage(storageKey);
+
+  if (storedValue?.value) {
+    inputAreaId.innerText = storedValue.value;
+    outputAreaId.value = decodeOrEncodeUrl(
+      buttonSwitch.innerText === textContent.switchEncoder,
+      inputAreaId.value
+    );
+  }
+  checkboxUseStorage.checked = !!storedValue?.isChecked;
+
+  /**
+   * Decode or encode on input or change of text in input area
+   */
+  inputAreaId.oninput = function () {
+    try {
+      outputAreaId.value = decodeOrEncodeUrl(
+        buttonSwitch.innerText === textContent.switchEncoder,
+        inputAreaId.value
+      );
+      inputAreaId.style.borderColor = "green";
+      checkboxUseStorage.checked &&
+        saveInStorage(storageKey, {
+          value: inputAreaId.value,
+          isChecked: checkboxUseStorage.checked,
+        });
+    } catch (error) {
+      inputAreaId.style.borderColor = "red";
+    }
+  };
+
+  /**
+   * save or remove from local storage on checkbox change
+   */
+  checkboxUseStorage.addEventListener('change', (event) => {
+    if (event.target.checked && inputAreaId.value) {
+      saveInStorage(storageKey, {
+        value: inputAreaId.value,
+        isChecked: checkboxUseStorage.checked,
+      });
+    } else {
+      removeFromStorage(storageKey);
+    }
+  });
+
+    /**
+     * Decoder to Encoder to Decoder switch button click handler
+     */
+    buttonSwitch.onclick = function () {
+      const wasPreviousStateDecoder= buttonSwitch.innerText === textContent.switchEncoder;
+      if (buttonSwitch.innerText === textContent.switchEncoder) {
+        buttonSwitch.innerText = textContent.switchDecoder;
+        toolName.innerText = textContent.encoderTool;
+      } else {
+        buttonSwitch.innerText = textContent.switchEncoder;
+        toolName.innerText = textContent.decoderTool;
+      }
+
+      //recompute output area value on switch
+      if (inputAreaId.value) {
+        outputAreaId.value =wasPreviousStateDecoder?inputAreaId.value: decodeOrEncodeUrl(
+          buttonSwitch.innerText === textContent.switchEncoder,
+          inputAreaId.value
+        );
+      }
+    };
+  };
